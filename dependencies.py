53adcdfd9008229a916a0b05f016c52a88247845
Yazan Jian <yazanjian@outlook.com>
2021-02-08 18:35:51 +0400

    All needed imports

---
+++ dependencies.py
@@ -0,0 +1,15 @@
+# Imports here
+import torch 
+import numpy as np
+
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from workspace_utils import active_session
+
+from PIL import Image
+
+import argparse
+
+import json
